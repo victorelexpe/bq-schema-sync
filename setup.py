7057de78dfc880b6e5497cd4e3da796c926f2434
Victor Hasim Elexpe Ahamri <victorelexpe@gmail.com>
2024-07-12 19:51:05 +0200

    Update version to 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
     from semantic_release.version import get_version
     version = get_version()
 except ImportError:
-    version = '0.1.7'
+    version = '0.1.8'
 
 setup(
     name='bq-schema-sync',
